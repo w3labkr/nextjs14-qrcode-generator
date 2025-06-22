e0c6a56c3721bede21cdfaf65dacb1a12c25cedf
W3LabKr <w3labkr@gmail.com>
2025-06-23 02:20:43 +0900

    feat: refactor AddressSearch component to use postcode popup for simplified address lookup

--- components/ui/address-search.tsx
+++ components/ui/address-search.tsx
@@ -1,16 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import DaumPostcode, { Address } from "react-daum-postcode";
+import { useDaumPostcodePopup, Address } from "react-daum-postcode";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogDescription,
-} from "@/components/ui/dialog";
 import { Search } from "lucide-react";
 
 interface AddressSearchProps {
@@ -19,7 +10,7 @@ interface AddressSearchProps {
 }
 
 export function AddressSearch({ onAddressSelect, children }: AddressSearchProps) {
-  const [isOpen, setIsOpen] = useState(false);
+  const openPostcode = useDaumPostcodePopup();
 
   const handleComplete = (data: Address) => {
     // 도로명 주소를 우선으로 사용, 없으면 지번 주소 사용
@@ -32,45 +23,22 @@ export function AddressSearch({ onAddressSelect, children }: AddressSearchProps)
     }
 
     onAddressSelect(completeAddress);
-    setIsOpen(false);
   };
 
-  return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger asChild>
-        {children ? (
-          <div onClick={() => setIsOpen(true)}>
-            {children}
-          </div>
-        ) : (
-          <Button
-            type="button"
-            variant="outline"
-            onClick={() => setIsOpen(true)}
-            className="w-full"
-          >
-            <Search className="h-4 w-4 mr-2" />
-            주소 검색
-          </Button>
-        )}
-      </DialogTrigger>
-      <DialogContent className="max-w-md max-h-[80vh] p-0">
-        <DialogHeader className="p-6 pb-0">
-          <DialogTitle>주소 검색</DialogTitle>
-          <DialogDescription>도로명 주소 및 지번 주소를 검색할 수 있습니다.</DialogDescription>
-        </DialogHeader>
-        <div className="p-6 pt-4">
-          <div className="min-h-[400px] w-full">
-            <DaumPostcode
-              onComplete={handleComplete}
-              style={{
-                width: "100%",
-                height: "400px",
-              }}
-            />
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
+  // 팝업 모드로 주소 검색 실행
+  return children ? (
+    <div onClick={() => openPostcode({ onComplete: handleComplete })}>
+      {children}
+    </div>
+  ) : (
+    <Button
+      type="button"
+      variant="outline"
+      onClick={() => openPostcode({ onComplete: handleComplete })}
+      className="w-full"
+    >
+      <Search className="h-4 w-4 mr-2" />
+      주소 검색
+    </Button>
   );
 }
